8f46e9eae7abf40f7a94120d852f8fb7bef17a05
Jaime Okhuysen <jamesokbo@gmail.com>
2017-03-01 23:13:51 +0000

    Fixed Models

--- server/models/mainRPi.js
+++ server/models/mainRPi.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 var mainRPiSchema= mongoose.Schema({
-    mainRPiID:String, /*Assigned by the server on identification*/
-    userID:String, /* The ID of the user to whom this system belongs to */
-    name:String, /* Assigned by the user to identify his Grove */
-    status: Boolean, /* true if connected to the server */
-    lastConnection: Date
+    mainRPiID:String, /* se asigna cuando se crea el documento, es entregado por el monitor */
+    name:String, /*nombre del monitor, se asigna por el usuario para identificar el cultivo monitoreado */
+    userID: {type:String, default:""}, /*Representa el usuario al que pertenece este monitor*/
+    status: {type:Boolean,default:false}, /*true si el monitor tiene una conexión abierta con el servidor, false de lo contrario*/
+    lastConnection: {type:Number,default:Date.now()}
 });
 
 module.exports=mongoose.model('MainRPi',mainRPiSchema);
-
